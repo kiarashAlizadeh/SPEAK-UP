0c00c0a7447475b4679f2562aee44233dc94aebd
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-09-24 13:38:20 +0330

    config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        Poppins: ['Poppins'],
+        Poppins: ["Poppins"],
       },
       colors: {
-        primary: 'rgb(37, 150, 190)',
-        'dark-primary': '#123276',
+        primary: "rgb(37, 150, 190)",
+        "dark-primary": "#123276",
       },
     },
   },
   plugins: [],
-};
+}
