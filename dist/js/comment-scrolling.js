ea2d9fd70d3ae508103aa892b50369165b964947
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-09-27 15:06:23 +0330

    js for commet scrooling updated

--- dist/js/comment-scrolling.js
+++ dist/js/comment-scrolling.js
@@ -1,6 +1,6 @@
 const scrollContainer = document.getElementById("sc")
 let scrollPosition = 0
-let scrollSpeed = window.innerWidth < 768 ? 1 : 0.5
+let scrollSpeed = window.innerWidth < 768 ? 1.5 : 0.5
 let isAutoScrolling = true
 
 function scrollImages() {
